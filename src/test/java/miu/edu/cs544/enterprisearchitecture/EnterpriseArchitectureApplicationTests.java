9f912f3020a2e971b4f07812c9779b5ba6ae2596
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-05-29 09:57:37 -0500

    init sbapp

---
+++ src/test/java/miu/edu/cs544/enterprisearchitecture/EnterpriseArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.cs544.enterprisearchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
