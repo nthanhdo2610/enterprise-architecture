4d31252c2f2c85bf49798739fb3c62fff505300a
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-06-07 11:14:29 -0500

    Queries & Optimization

--- lectures/w2d2/W2D2-JPQL/src/main/java/cs544/App.java
+++ lectures/w2d2/W2D2-JPQL/src/main/java/cs544/App.java
@@ -1,16 +1,11 @@
 package cs544;
 
-import java.util.List;
-
-import cs544.model.Airline;
-import cs544.model.Flight;
-import java.text.DateFormat;
-import java.util.Locale;
-
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import jakarta.persistence.TypedQuery;
+import jakarta.persistence.Query;
+
+import java.util.List;
 
 public class App {
 
@@ -18,84 +13,99 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         emf = Persistence.createEntityManagerFactory("cs544");
-        		
+        questionA("USA", 500);
+
+//        EntityManager em = emf.createEntityManager();
+//        em.getTransaction().begin();
+//
+//        // b) TODO: All airlines that use A380 airplanes
+//        System.out.println("Question B:");
+//        List<Airline> airlines = em.createQuery("from Airline ", Airline.class).getResultList();
+//        System.out.println("Airlines that use A380s:");
+//        for (Airline airline : airlines) {
+//            System.out.println(airline.getName());
+//        }
+//        System.out.println();
+//
+//        em.getTransaction().commit();
+//        em.close();
+//
+//        em = emf.createEntityManager();
+//        em.getTransaction().begin();
+//
+//        // c) TODO: Flights using 747 planes that don't belong to Star Alliance
+//        System.out.println("Question C:");
+//        flights = em.createQuery("from Flight ", Flight.class).getResultList();
+//        System.out.printf("%-9s%-31s%-31s\n", "Flight:", "Departs:",
+//                "Arrives:");
+//        for (Flight flight : flights) {
+//            System.out.printf("%-7s  %-12s %7s %8s  %-12s %7s %8s\n",
+//                    flight.getFlightnr(), flight.getOrigin().getCity(),
+//                    flight.getDepartureDate(), flight.getDepartureTime(),
+//                    flight.getDestination().getCity(),
+//                    flight.getArrivalDate(), flight.getArrivalTime());
+//        }
+//        System.out.println();
+//
+//        em.getTransaction().commit();
+//        em.close();
+//
+//        em = emf.createEntityManager();
+//        em.getTransaction().begin();
+//
+//        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT,
+//                Locale.US);
+//        DateFormat tf = DateFormat.getTimeInstance(DateFormat.SHORT,
+//                Locale.US);
+//
+//        // d) TODO: All flights leaving before 12pm on 08/07/2009
+//        System.out.println("Question D:");
+//        TypedQuery<Flight> query = em.createQuery("from Flight ", Flight.class);
+//        flights = query.getResultList();
+//        System.out.printf("%-9s%-31s%-31s\n", "Flight:", "Departs:",
+//                "Arrives:");
+//        for (Flight flight : flights) {
+//            System.out.printf("%-7s  %-12s %7s %8s  %-12s %7s %8s\n",
+//                    flight.getFlightnr(), flight.getOrigin().getCity(),
+//                    flight.getDepartureDate(), flight.getDepartureTime(),
+//                    flight.getDestination().getCity(),
+//                    flight.getArrivalDate(), flight.getArrivalTime());
+//        }
+//        System.out.println();
+//        em.getTransaction().commit();
+//        em.close();
+    }
+
+    static void questionA(String country, int cap){
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
-
         // a) TODO: Flights leaving USA capacity > 500
+        /**
+         * select f.* from Flight f
+         *     inner join Airport a on f.origin_id = a.id and a.country = 'USA'
+         *     inner join Airplane p on f.airplane_id = p.id and p.capacity > 500;
+         */
         System.out.println("Question A:");
-        List<Flight> flights = em.createQuery("from Flight ", Flight.class).getResultList();
+        Query query = em.createNativeQuery("select f.* from Flight f " +
+                "inner join Airport a on f.origin_id = a.id and a.country = :country " +
+                "inner join Airplane p on f.airplane_id = p.id and p.capacity > :cap");
+        query.setParameter("country", country);
+        query.setParameter("cap", cap);
+        List<Object[]> flights = query.getResultList();
         System.out.printf("%-9s%-31s%-31s\n", "Flight:", "Departs:",
                 "Arrives:");
-        for (Flight flight : flights) {
-            System.out.printf("%-7s  %-12s %7s %8s  %-12s %7s %8s\n",
-                    flight.getFlightnr(), flight.getOrigin().getCity(),
-                    flight.getDepartureDate(), flight.getDepartureTime(),
-                    flight.getDestination().getCity(),
-                    flight.getArrivalDate(), flight.getArrivalTime());
-        }
+//        for (FlightDTO flight : flights) {
+//            System.out.printf("%-7s  %-12s %7s %8s  %-12s %7s %8s\n",
+//                    flight.getFlightnr(),
+//                    flight.getOrigin().getCity(),
+//                    flight.getDepartureDate(),
+//                    flight.getDepartureTime(),
+//                    flight.getDestination().getCity(),
+//                    flight.getArrivalDate(),
+//                    flight.getArrivalTime());
+//        }
         System.out.println();
 
-        em.getTransaction().commit();
-        em.close();
-
-        em = emf.createEntityManager();
-        em.getTransaction().begin();
-
-        // b) TODO: All airlines that use A380 airplanes
-        System.out.println("Question B:");
-        List<Airline> airlines = em.createQuery("from Airline ", Airline.class).getResultList();
-        System.out.println("Airlines that use A380s:");
-        for (Airline airline : airlines) {
-            System.out.println(airline.getName());
-        }
-        System.out.println();
-
-        em.getTransaction().commit();
-        em.close();
-
-        em = emf.createEntityManager();
-        em.getTransaction().begin();
-
-        // c) TODO: Flights using 747 planes that don't belong to Star Alliance
-        System.out.println("Question C:");
-        flights = em.createQuery("from Flight ", Flight.class).getResultList();
-        System.out.printf("%-9s%-31s%-31s\n", "Flight:", "Departs:",
-                "Arrives:");
-        for (Flight flight : flights) {
-            System.out.printf("%-7s  %-12s %7s %8s  %-12s %7s %8s\n",
-                    flight.getFlightnr(), flight.getOrigin().getCity(),
-                    flight.getDepartureDate(), flight.getDepartureTime(),
-                    flight.getDestination().getCity(),
-                    flight.getArrivalDate(), flight.getArrivalTime());
-        }
-        System.out.println();
-
-        em.getTransaction().commit();
-        em.close();
-
-        em = emf.createEntityManager();
-        em.getTransaction().begin();
-
-        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT,
-                Locale.US);
-        DateFormat tf = DateFormat.getTimeInstance(DateFormat.SHORT,
-                Locale.US);
-
-        // d) TODO: All flights leaving before 12pm on 08/07/2009
-        System.out.println("Question D:");
-        TypedQuery<Flight> query = em.createQuery("from Flight ", Flight.class);
-        flights = query.getResultList();
-        System.out.printf("%-9s%-31s%-31s\n", "Flight:", "Departs:",
-                "Arrives:");
-        for (Flight flight : flights) {
-            System.out.printf("%-7s  %-12s %7s %8s  %-12s %7s %8s\n",
-                    flight.getFlightnr(), flight.getOrigin().getCity(),
-                    flight.getDepartureDate(), flight.getDepartureTime(),
-                    flight.getDestination().getCity(),
-                    flight.getArrivalDate(), flight.getArrivalTime());
-        }
-        System.out.println();
         em.getTransaction().commit();
         em.close();
     }
