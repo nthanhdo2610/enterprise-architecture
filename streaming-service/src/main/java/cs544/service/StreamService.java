09420a6644a38bb3872d3f69a12fdc2c787a6e6f
Bayasaa <bayarbayasgalan.j@gmail.com>
2023-06-16 15:48:19 -0500

    remove some no needed things

--- streaming-service/src/main/java/cs544/service/StreamService.java
+++ streaming-service/src/main/java/cs544/service/StreamService.java
@@ -1,31 +1,14 @@
 package cs544.service;
 
 import cs544.StreamSender;
-import cs544.dao.StreamDao;
-import cs544.domain.Game;
-
-import org.springframework.data.mongodb.core.ChangeStreamOptions;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.messaging.ChangeStreamRequest;
-import org.springframework.data.mongodb.core.messaging.DefaultMessageListenerContainer;
-import org.springframework.data.mongodb.core.messaging.MessageListenerContainer;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import jakarta.annotation.PostConstruct;
-
 @Service
 @Transactional
 public class StreamService {
-    private MongoTemplate mongoTemplate;
-    private final StreamDao gameDao;
     private final StreamSender streamSender;
 
-    public StreamService(StreamDao gameDao, MongoTemplate mongoTemplate, StreamSender streamSender) {
-        this.gameDao = gameDao;
-        this.mongoTemplate = mongoTemplate;
+    public StreamService(StreamSender streamSender) {
         this.streamSender = streamSender;
     }
 
