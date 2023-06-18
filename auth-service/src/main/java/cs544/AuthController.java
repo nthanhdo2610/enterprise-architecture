f9054c384398dbdfd8659dd905dfc06805160382
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-06-18 15:25:03 -0500

    build docker-compose multiple services

--- auth-service/src/main/java/cs544/AuthController.java
+++ auth-service/src/main/java/cs544/AuthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
 
-    @GetMapping
+    @GetMapping("/testing")
     public String getAll() {
         return "AuthController";
     }
--- streaming-service/src/main/java/cs544/StreamingController.java
+++ streaming-service/src/main/java/cs544/StreamingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StreamingController {
 
-    @GetMapping
+    @GetMapping("/testing")
     public String getAll() {
         return "StreamingController";
     }
