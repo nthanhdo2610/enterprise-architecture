7cfc013e2eaed539d6f3517181bc6f175242b1ab
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-06-18 11:55:02 -0500

    build docker-compose multiple services

---
+++ streaming-service/src/main/java/cs544/RabbitConfiguration.java
@@ -0,0 +1,16 @@
+package cs544;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitConfiguration {
+
+    private static final String QUEUE_NAME = "stream_transfer";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME);
+    }
+}
