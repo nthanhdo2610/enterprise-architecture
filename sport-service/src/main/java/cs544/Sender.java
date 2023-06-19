3983b8d93b4953560dd088f011815b943915a186
Bayasaa <bayarbayasgalan.j@gmail.com>
2023-06-19 14:35:30 -0500

    removed implements commandliner

--- sport-service/src/main/java/cs544/Sender.java
+++ sport-service/src/main/java/cs544/Sender.java
@@ -2,11 +2,10 @@ package cs544;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Sender implements CommandLineRunner {
+public class Sender{
     @Autowired
     private RabbitTemplate template;
     
@@ -15,8 +14,4 @@ public class Sender implements CommandLineRunner {
         System.out.println("Sent: " + message + " to: " + queue);
     }
     
-    @Override
-    public void run(String... args) throws Exception {
-        
-    }
 }
--- sport-service/src/main/java/cs544/SportApplication.java
+++ sport-service/src/main/java/cs544/SportApplication.java
@@ -3,24 +3,18 @@ package cs544;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
-public class SportApplication implements CommandLineRunner{
+public class SportApplication{
     @Autowired
     private MongoTemplate mongoTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(SportApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("----------------"+mongoTemplate.getCollectionNames());
     }
 
     @Bean
